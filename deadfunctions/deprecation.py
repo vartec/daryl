aeb73e190c69817bd5d0a73ddf8fdba13b3a1894
Bartek Ogryczak <bartek@eventbrite.com>
2015-03-27 14:02:44 -0700

    deprecation decorator

---
+++ deadfunctions/deprecation.py
@@ -0,0 +1,23 @@
+import functools
+from logging import getLogger
+
+logger = getLogger("deprecation")
+
+
+def deprecated(author, date):
+    """
+        Function to mark code as suspected to be dead.
+        If function is ever used, a warning will be logged to Sentry
+        usage:
+
+        @deprecated('your_name', '2015-03-27')
+        def zombie_function(...):
+    """
+    def _decorator(f):
+        @functools.wraps(f)
+        def _wrapped(*args, **kwargs):
+            logger.warn("Deprecated function called! {f_name} (deprecated by {author} on {date})"
+                        .format({"f_name": f.__name__, "author": author, "date": date}), ext_info=True)
+            return f(*args, **kwargs)
+        return _wrapped
+    return _decorator
